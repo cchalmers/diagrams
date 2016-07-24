fb5b4a0d763f1ffc603248de642fbb93028b6534
Christopher Chalmers <c.chalmers@me.com>
2016-07-24 15:09:05 +0100

    Remove foldDia'
    It's not needed now clip is on a static annotation. Also viewing down
    annotations midway can be missleading because of prioity/backup
    attributes.

--- src/Diagrams/Backend/Compile.hs
+++ src/Diagrams/Backend/Compile.hs
@@ -34,7 +34,7 @@ import           Control.Lens              hiding (transform)
 import qualified Data.Foldable             as F
 import           Data.Monoid.Coproduct
 import qualified Data.Monoid as            M
-import           Data.Tree.DUAL.Label      (foldDUAL, foldDUAL')
+import           Data.Tree.DUAL.Label      (foldDUAL)
 import           Data.Typeable
 
 import           Geometry.Envelope    (size)
@@ -73,33 +73,6 @@ foldDiaWithScales primF aF g n (QD dual) = foldDUAL lF aF dual
         in  foldDUAL lF aF dia
 {-# INLINE foldDiaWithScales #-}
 
-foldDiaWithScales'
-  :: (HasLinearMap v, OrderedField n, M.Monoid r)
-  => (Transformation v n -> Attributes -> Prim v n -> r) -- ^ render a prim
-  -> (Annotation v n -> r -> r) -- ^ render an attribute
-  -> (Attributes -> r -> r) -- ^ render a "overlooking" attributes
-  -> n -- 'global' to 'output' scale factor
-  -> n -- 'normalised' to 'output' scale factor
-  -> QDiagram v n m -- ^ diagram to fold
-  -> r
-foldDiaWithScales' primF aF styF g n (QD dual) = foldDUAL' lF aF pF dual
-  where
-    lF d = \case
-      PrimLeaf p    ->
-        let (tr, sty) = untangle d
-        in  primF tr (getAttributes g n sty) p
-      DelayedLeaf f ->
-        let (QD dia) = f d g n
-        in  foldDUAL' lF aF pF dia
-
-    -- The partial sty needs the total transform accumilated so far, but
-    -- ignores any style before.
-    pF d w = styF attrs where
-      attrs    = getAttributes g n $ transform t sty
-      t        = killR d
-      (_, sty) = untangle w
-{-# INLINE foldDiaWithScales' #-}
-
 -- | Simple way to fold a diagram into a monoidal result.
 foldDia
   :: (HasLinearMap v, OrderedField n, M.Monoid r)
@@ -113,26 +86,6 @@ foldDia primF annF t d = foldDiaWithScales primF annF g n d
     g = avgScale t
     n = normalizedFactor (size d)
 {-# INLINE foldDia #-}
-
--- | Fold a diagram into a monoidal result. Similar to 'foldDia' but
---   gives access to the style when it's higher up the tree. This is
---   useful for things like clipping where you want to use the same
---   clipping for everything below that point. This is reset after each
---   group and given as the second argument in the prim rendering
---   function.
-foldDia'
-  :: (HasLinearMap v, OrderedField n, M.Monoid r)
-  => (Transformation v n -> Attributes -> Prim v n -> r)
-  -> (Annotation v n -> r -> r)
-  -> (Attributes -> r -> r)
-  -> Transformation v n
-  -> QDiagram v n m -- ^ diagram to fold
-  -> r
-foldDia' primF annF styF t d = foldDiaWithScales' primF annF styF g n d
-  where
-    g = avgScale t
-    n = normalizedFactor (size d)
-{-# INLINE foldDia' #-}
 
 -- Standard prisms -----------------------------------------------------
 
@@ -164,18 +117,6 @@ _ExternalImage = _Prim
 --   transform.
 normalizedFactor :: (F.Foldable v, Floating n) => v n -> n
 normalizedFactor v = F.product v ** (1 / fromIntegral (lengthOf folded v))
-
--- | Render a diagram, returning also the transformation which was
---   used to convert the diagram from its (\"global\") coordinate
---   system into the output coordinate system.  The inverse of this
---   transformation can be used, for example, to convert output/screen
---   coordinates back into diagram coordinates.  See also 'adjustDia'.
--- renderDiaT
---   :: (Backend b v n , HasLinearMap v, Metric v, Typeable1 v,
---       Typeable n, OrderedField n, Monoid' m)
---   => Options b v n -> QDiagram v n m -> (Transformation v n, Result b v n)
--- renderDiaT b opts d = (g2o, renderDUAL b opts' g2o d')
---   where (opts', g2o, d') = adjustDia b opts d
 
 -- | Render a diagram.
 -- renderDia
