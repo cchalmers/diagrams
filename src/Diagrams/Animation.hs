33b0a1078ea77f9af45e5d93aa29d9ae3c5b2a00
Christopher Chalmers <c.chalmers@me.com>
2016-08-05 19:14:20 +0100

    Add animation and active

---
+++ src/Diagrams/Animation.hs
@@ -0,0 +1,129 @@
+{-# LANGUAGE CPP                   #-}
+{-# LANGUAGE FlexibleContexts      #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeFamilies          #-}
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Diagrams.Animation
+-- Copyright   :  (c) 2011 diagrams-lib team (see LICENSE)
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  diagrams-discuss@googlegroups.com
+--
+-- An animation is a time-varying diagram, together with start and end
+-- times.  Most of the tools for working with animations can actually
+-- be found in the @active@ package, which defines the 'Active' type.
+--
+-- XXX more documentation and examples should go here
+--
+-----------------------------------------------------------------------------
+
+module Diagrams.Animation
+  ( -- * Types for animations
+    QAnimation
+  , Animation
+
+    -- * Animation combinators and tools
+    -- $animComb
+  , animEnvelope, animEnvelope'
+
+  , animRect, animRect'
+
+  ) where
+
+import           Data.Active
+#if __GLASGOW_HASKELL__ < 710
+import           Control.Applicative       ((<$>))
+import           Data.Foldable             (foldMap)
+#endif
+
+import Geometry
+import Diagrams.Types
+import Data.Monoid.WithSemigroup
+import           Diagrams.Animation.Active ()
+
+-- | A value of type @QAnimation b v m@ is an animation (a
+--   time-varying diagram with start and end times) that can be
+--   rendered by backspace @b@, with vector space @v@ and monoidal
+--   annotations of type @m@.
+type QAnimation v n m = Active (QDiagram v n m)
+
+-- | A value of type @Animation b v@ is an animation (a time-varying
+--   diagram with start and end times) in vector space @v@ that can be
+--   rendered by backspace @b@.
+--
+--   Note that @Animation@ is actually a synonym for @QAnimation@
+--   where the type of the monoidal annotations has been fixed to
+--   'Any' (the default).
+type Animation v = Active (Diagram v)
+
+-- $animComb
+-- Most combinators for working with animations are to be found in the
+-- @active@ package, which defines the 'Active' type.  This module
+-- defines just a few combinators specifically for working with
+-- animated diagrams.
+
+-- It would be cool to have a variant of animEnvelope that tries to do
+-- some sort of smart adaptive sampling to get good results more
+-- quickly.  One could also imagine trying to use some sort of
+-- automatic differentiation but that probably wouldn't work in all
+-- cases we want to handle.
+
+-- | Automatically assign fixed a envelope to the entirety of an
+--   animation by sampling the envelope at a number of points in time
+--   and taking the union of all the sampled envelopes to form the
+--   \"hull\".  This hull is then used uniformly throughout the
+--   animation.
+--
+--   This is useful when you have an animation that grows and shrinks
+--   in size or shape over time, but you want it to take up a fixed
+--   amount of space, /e.g./ so that the final rendered movie does not
+--   zoom in and out, or so that it occupies a fixed location with
+--   respect to another animation, when combining animations with
+--   something like '|||'.
+--
+--   By default, 30 samples per time unit are used; to adjust this
+--   number see 'animEnvelope''.
+--
+--   See also 'animRect' for help constructing a background to go
+--   behind an animation.
+animEnvelope
+  :: (OrderedField n, HasLinearMap v, Monoid' m)
+  => QAnimation v n m -> QAnimation v n m
+animEnvelope = animEnvelope' 30
+
+-- | Like 'animEnvelope', but with an adjustible sample rate. The first
+--   parameter is the number of samples per time unit to use. Lower
+--   rates will be faster but less accurate; higher rates are more
+--   accurate but slower.
+animEnvelope'
+  :: (OrderedField n, HasLinearMap v, Monoid' m)
+  => Rational -> QAnimation v n m -> QAnimation v n m
+-- animEnvelope' r a = withEnvelope (simulate r a) <$> a
+animEnvelope' r a = modEnvelope (const . getEnvelope $ simulate r a) <$> a
+
+-- | @animRect@ works similarly to 'animEnvelope' for 2D diagrams, but
+--   instead of adjusting the envelope, simply returns the smallest
+--   bounding rectangle which encloses the entire animation.  Useful
+--   for /e.g./ creating a background to go behind an animation.
+--
+--   Uses 30 samples per time unit by default; to adjust this number
+--   see 'animRect''.
+animRect
+  :: (InSpace V2 n t, Monoid' m, TrailLike t)
+  => QAnimation V2 n m -> t
+animRect = trailLike . animRect' 30
+
+-- | Like 'animRect', but with an adjustible sample rate.  The first
+--   parameter is the number of samples per time unit to use. Lower
+--   rates will be faster but less accurate; higher rates are more
+--   accurate but slower.
+animRect'
+  :: (InSpace V2 n t, Monoid' m, TrailLike t)
+  => Rational -> QAnimation V2 n m -> t
+animRect' r anim =
+  case boxTransform (boundingBox results) (fromCorners (-0.5) 0.5) of
+    Just t  -> trailLike $ transform t (rect 1 1)
+    Nothing -> rect 1 1
+  where
+    results = simulate r anim
+
---
+++ src/Diagrams/Animation/Active.hs
@@ -0,0 +1,104 @@
+{-# LANGUAGE CPP          #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# OPTIONS_GHC -fno-warn-orphans #-}
+
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Diagrams.Animation.Active
+-- Copyright   :  (c) 2011 Brent Yorgey
+-- License     :  BSD-style (see LICENSE)
+-- Maintainer  :  byorgey@cis.upenn.edu
+--
+-- A few utilities and class instances for 'Active' (from the @active@
+-- package).  In particular, this module defines
+--
+--   * An instance of 'V' for 'Active': @'V' ('Active' a) = 'V' a@
+--
+--   * 'HasOrigin', 'Transformable', and 'HasStyle' instances for
+--     'Active' which all work pointwise.
+--
+--   * A 'TrailLike' instance for @'Active' p@ where @p@ is also
+--     'TrailLike', which simply lifts a pathlike thing to a constant
+--     active value.
+--
+--   * A 'Juxtaposable' instance for @'Active' a@ where @a@ is also
+--     'Juxtaposable'.  An active value can be juxtaposed against
+--     another by doing the juxtaposition pointwise over time.  The
+--     era of @juxtapose v a1 a2@ will be the same as the era of @a2@,
+--     unless @a2@ is constant, in which case it will be the era of
+--     @a1@.  (Note that @juxtapose v a1 a2@ and @liftA2 (juxtapose v)
+--     a1 a2@ therefore have different semantics: the second is an
+--     active value whose era is the /combination/ of the eras of @a1@
+--     and @a2@).
+--
+--   * An 'Alignable' instance for @'Active' a@ where @a@ is also
+--     'Alignable'; the active value is aligned pointwise over time.
+
+-----------------------------------------------------------------------------
+
+module Diagrams.Animation.Active where
+
+#if __GLASGOW_HASKELL__ < 710
+import           Control.Applicative (pure, (<$>))
+#endif
+
+import           Geometry
+
+import Diagrams.Types.Style
+
+import           Data.Active
+
+type instance V (Active a) = V a
+type instance N (Active a) = N a
+
+-- Yes, these are all orphan instances. Get over it.  We don't want to
+-- put them in the 'active' package because 'active' is supposed to be
+-- generally useful and shouldn't depend on diagrams.  We'd also
+-- rather not put them in diagrams-core so that diagrams-core doesn't
+-- have to depend on active.
+
+instance HasOrigin a => HasOrigin (Active a) where
+  moveOriginTo = fmap . moveOriginTo
+
+instance Transformable a => Transformable (Active a) where
+  transform = fmap . transform
+
+instance ApplyStyle a => ApplyStyle (Active a) where
+  applyStyle = fmap . applyStyle
+
+instance TrailLike t => TrailLike (Active t) where
+  trailLike = pure . trailLike
+
+-- | An active value can be juxtaposed against another by doing the
+--   juxtaposition pointwise over time.  The era of @juxtapose v a1
+--   a2@ will be the same as the era of @a2@, unless @a2@ is constant,
+--   in which case it will be the era of @a1@.  (Note that @juxtapose
+--   v a1 a2@ and @liftA2 (juxtapose v) a1 a2@ therefore have
+--   different semantics: the second is an active value whose era is
+--   the /combination/ of the eras of @a1@ and @a2@).
+instance Juxtaposable a => Juxtaposable (Active a) where
+
+  juxtapose v a1 a2 =
+    onActive       -- a1
+      (\c1 ->        -- if a1 is constant, just juxtapose a2 pointwise with its value
+        juxtapose v c1 <$> a2
+      )
+                     -- if a1 is dynamic...
+      (onDynamic $ \s1 e1 d1 ->
+        onActive      -- a2
+          (\c2 ->      -- if a2 is constant, juxtapose pointwise with a1.  Since
+                       --   the result will no longer be constant, the result
+                       --   needs an era: we use a1's.
+            mkActive s1 e1 (\t -> juxtapose v (d1 t) c2)
+          )
+
+                       -- otherwise, juxtapose pointwise, without changing a2's era
+          (onDynamic $ \s2 e2 d2 ->
+            mkActive s2 e2 (\t -> juxtapose v (d1 t) (d2 t))
+          )
+          a2
+      )
+      a1
+
+-- instance Alignable a => Alignable (Active a) where
+--   alignBy v d a = alignBy v d <$> a
